0c4ebd0099ee92790e710ae49b49277fdb7adf35
yerko.a.vera.lezama <1311539_y3rk0>
2017-11-17 20:05:32 -0500

    Initial commit

---
+++ sample/src/main/java/com/yarolegovich/slidingrootnav/sample/SampleActivity.java
@@ -0,0 +1,173 @@
+package com.yarolegovich.slidingrootnav.sample;
+
+import android.app.Fragment;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.MapboxMapOptions;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.SupportMapFragment;
+import com.mapbox.services.android.navigation.v5.navigation.MapboxNavigation;
+import com.yarolegovich.slidingrootnav.SlidingRootNav;
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
+import com.yarolegovich.slidingrootnav.sample.menu.DrawerAdapter;
+import com.yarolegovich.slidingrootnav.sample.menu.DrawerItem;
+import com.yarolegovich.slidingrootnav.sample.menu.SimpleItem;
+import com.yarolegovich.slidingrootnav.sample.menu.SpaceItem;
+import com.yarolegovich.slidingrootnav.sample.fragment.CenteredTextFragment;
+
+import java.util.Arrays;
+
+/**
+ * Created by yarolegovich on 25.03.2017.
+ */
+
+public class SampleActivity extends AppCompatActivity implements DrawerAdapter.OnItemSelectedListener {
+
+    private static final int POS_DASHBOARD = 0;
+    private static final int POS_ACCOUNT = 1;
+    private static final int POS_MESSAGES = 2;
+    private static final int POS_CART = 3;
+    private static final int POS_LOGOUT = 5;
+
+    private String[] screenTitles;
+    private Drawable[] screenIcons;
+
+    private SlidingRootNav slidingRootNav;
+
+    FragmentTransaction transaction;
+    SupportMapFragment mapFragment;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Mapbox.getInstance(this, getString(R.string.token_app));
+        MapboxNavigation navigation = new MapboxNavigation(this, getString(R.string.token_app));
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (savedInstanceState == null) {
+
+            // Create fragment
+            transaction = getSupportFragmentManager().beginTransaction();
+
+            LatLng patagonia = new LatLng(-12.0432, -77.0283);
+
+            // Build mapboxMap
+            MapboxMapOptions options = new MapboxMapOptions();
+            options.styleUrl(Style.MAPBOX_STREETS);
+            options.camera(new CameraPosition.Builder()
+                    .target(patagonia)
+                    .zoom(9)
+                    .build());
+
+            // Create map fragment
+            mapFragment = SupportMapFragment.newInstance(options);
+
+            // Add map fragment to parent container
+            transaction.add(R.id.container, mapFragment, "com.mapbox.map");
+            transaction.commit();
+        } else {
+            mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentByTag("com.mapbox.map");
+        }
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                // Customize map with markers, polylines, etc.
+            }
+        });
+
+        slidingRootNav = new SlidingRootNavBuilder(this)
+                .withToolbarMenuToggle(toolbar)
+                .withMenuOpened(false)
+                .withContentClickableWhenMenuOpened(false)
+                .withSavedState(savedInstanceState)
+                .withMenuLayout(R.layout.menu_left_drawer)
+                .inject();
+
+        screenIcons = loadScreenIcons();
+        screenTitles = loadScreenTitles();
+
+        DrawerAdapter adapter = new DrawerAdapter(Arrays.asList(
+                createItemFor(POS_DASHBOARD).setChecked(true),
+                createItemFor(POS_ACCOUNT),
+                createItemFor(POS_MESSAGES),
+                createItemFor(POS_CART),
+                new SpaceItem(48),
+                createItemFor(POS_LOGOUT)));
+        adapter.setListener(this);
+
+        RecyclerView list = findViewById(R.id.list);
+        list.setNestedScrollingEnabled(false);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(adapter);
+
+        adapter.setSelected(POS_DASHBOARD);
+    }
+
+    @Override
+    public void onItemSelected(int position) {
+        if (position == POS_LOGOUT) {
+            finish();
+        }else if(position == POS_ACCOUNT){
+
+        }
+        slidingRootNav.closeMenu();
+        Fragment selectedScreen = CenteredTextFragment.createFor(screenTitles[position]);
+        showFragment(selectedScreen);
+    }
+
+    private void showFragment(Fragment fragment) {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+    }
+
+    private DrawerItem createItemFor(int position) {
+        return new SimpleItem(screenIcons[position], screenTitles[position])
+                .withIconTint(color(R.color.textColorSecondary))
+                .withTextTint(color(R.color.textColorPrimary))
+                .withSelectedIconTint(color(R.color.colorAccent))
+                .withSelectedTextTint(color(R.color.colorAccent));
+    }
+
+    private String[] loadScreenTitles() {
+        return getResources().getStringArray(R.array.ld_activityScreenTitles);
+    }
+
+    private Drawable[] loadScreenIcons() {
+        TypedArray ta = getResources().obtainTypedArray(R.array.ld_activityScreenIcons);
+        Drawable[] icons = new Drawable[ta.length()];
+        for (int i = 0; i < ta.length(); i++) {
+            int id = ta.getResourceId(i, 0);
+            if (id != 0) {
+                icons[i] = ContextCompat.getDrawable(this, id);
+            }
+        }
+        ta.recycle();
+        return icons;
+    }
+
+    @ColorInt
+    private int color(@ColorRes int res) {
+        return ContextCompat.getColor(this, res);
+    }
+}
